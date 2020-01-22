a0fae4793be298207672fa7c496c5fb456d0e36a
efecihancigdem <cefecigdem@gmail.com>
2020-01-22 00:44:01 -0500

    Added main file

---
+++ SudokuSolver.py
@@ -0,0 +1,28 @@
+#declare Map variable
+Map = [[7,1,"x",8,"x",5,4,"x",9]
+      ,[9,"x",4,6,2,7,3,1,"x"]
+      ,["x","x","x",4,"x",9,5,7,6]
+      ,["x","x","x","x",8,3,7,4,"x"]
+      ,[4,"x","x","x",5,6,9,8,1]
+      ,[8,7,1,9,"x",2,6,3,"x"]
+      ,[1,"x",9,5,7,4,2,6,3]
+      ,[3,"x",7,2,"x",8,1,5,4]
+      ,[5,4,2,3,6,1,8,"x","x"]]
+
+full_line = [x for x in range(1,10)]
+
+#Horizontal check
+
+
+
+#Verical Check
+
+
+#Square Check
+
+
+
+
+#Run it in a for while loop if nothing is changing stop to avoid endless run
+
+
